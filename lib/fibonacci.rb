530cf09ad5ad12f0bce41ab01ddb6cce600618eb
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-26 14:15:43 -0400

    Add project files

---
+++ lib/fibonacci.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+def fibs(num)
+  # The iteration solution goes here
+end
+
+def fibs_rec(num)
+  # The recursive solution goes here
+end
---
+++ lib/merge_sort.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def merge_sort(arr)
+  # The one and only solution (recursive) goes here
+end
