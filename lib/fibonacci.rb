6339fd56851c510f9fe722d481e86cade08ce06f
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-27 15:18:20 -0400

    Add recursive solution for Fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -13,6 +13,14 @@ def fibs(num)
   results
 end
 
-def fibs_rec(num)
-  # The recursive solution goes here
+def fibs_rec(num, results = [])
+  return results if num < 1
+  return [0] if num == 1 && results.empty?
+
+  results.push(0, 1) && num -= 2 if results.empty?
+  return results if num.zero?
+
+  size = results.length
+  results << results[size - 1] + results[size - 2]
+  fibs_rec(num - 1, results)
 end
