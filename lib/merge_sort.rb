a8ec5bee95370a84b500e6ade5573cc5de4fb78b
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-01 15:49:50 -0400

    Rework splitting functionality

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
-def merge_sort(arr)
-  # The one and only solution (recursive) goes here
+def merge_sort(arr = [])
+  return arr if arr.length < 2
+
+  center = arr.length / 2
+  center += 1 if arr.length.odd?
+  left = merge_sort(arr.slice(0, center))
+  right = merge_sort(arr.slice(center, arr.length))
 end
+
+merge_sort([4, 3, 2, 5, 1, 6, 7])
